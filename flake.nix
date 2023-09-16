a89828178dced5b37d6b1d8178169da072042d6b
Denis Lehmann <denis.lehmann@4am-robotics.com>
2023-09-16 13:53:34 +0200

    add flake

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  description = "Get status information about multiple Git repositories";
+  outputs = { self, nixpkgs }@inputs:
+    let
+      forAllSystems = nixpkgs.lib.genAttrs nixpkgs.lib.platforms.unix;
+
+      nixpkgsFor = forAllSystems (system: import nixpkgs {
+        inherit system;
+      });
+    in
+      {
+        packages = forAllSystems (system:
+          let pkgs = nixpkgsFor.${system}; in
+          {
+            gis = pkgs.stdenv.mkDerivation {
+              name = "gis";
+              src = self;
+              installPhase = ''
+                install -m 755 -D gis $out/bin/gis
+              '';
+            };
+            default = self.packages.${system}.gis;
+        });
+      };
+}
